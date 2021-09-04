35010c3f6eac9e9244c9adb1fde8735e263a4c62
Abdulaziz1234567 <87083669+Abdulaziz1234567@users.noreply.github.com>
2021-09-04 12:44:04 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,52 @@
+
+$('.header__nav-1').hide();
+$('.header__menu-1').hide();
+$('.header__tema-1').hide();
+$('.header__number-1').hide();
+
+
+$(function () {
+$('.btn-1').on('click', function () {
+    $('.header__nav-1').slideToggle(1000);
+    $('.header__menu-1').fadeToggle(1000);
+    $('.header__tema-1').fadeToggle(1000);
+    $('.header__number-1').fadeToggle(1000);
+    
+  
+})
+})
+$('.main__page-1').hide();
+$(function () {
+$('.page1_btn_1').on('click', function () {
+    $('.main__page-1').slideToggle(1000);
+ 
+    
+  
+})
+})
+$('.section__input-page').hide();
+$(function () {
+$('.page1_btn').on('click', function () {
+    $('.section__input-page-1').slideToggle(1000);
+
+    
+  
+})
+})
+$('.section__input-page-1').hide();
+$(function () {
+$('.input_btn').on('click', function () {
+    $('.section__input-page-1').slideToggle(1000);
+
+    
+  
+})
+})
+$(function () {
+$('.btn11').on('click', function () {
+    $('.section__bootom-input').slideToggle(1000);
+
+    
+  
+})
+})
